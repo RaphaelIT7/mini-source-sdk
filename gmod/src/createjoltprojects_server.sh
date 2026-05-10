09986bdf1ad2a2f8188c70fb088422cb1ecb4c0a
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-05-10 05:54:26 +0200

    gmod: lazy fix 64x by dropping DEDICATED again

--- gmod/src/createjoltprojects_server.sh
+++ gmod/src/createjoltprojects_server.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "64" ]; then
   echo "64x Build"
-  devtools/bin/vpc +jolt /linux64 /define:PLATFORM_64BITS /define:GAME_SDK2013 /define:GAME_GMOD /define:SWDS /define:DEDICATED /mksln jolt
+  devtools/bin/vpc +jolt /linux64 /define:PLATFORM_64BITS /define:GAME_SDK2013 /define:GAME_GMOD /define:SWDS /mksln jolt
 else
   echo "32x Build"
   devtools/bin/vpc +jolt /define:GAME_SDK2013 /define:GAME_GMOD /define:SWDS /define:DEDICATED /mksln jolt
